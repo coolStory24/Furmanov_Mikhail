76f7efcd3a7acaa8ecf816b78508b2c3f2764caa
coolStory24 <mihail.furm@gmail.com>
2023-09-26 20:55:33 +0000

    add maven

--- zoo/src/main/java/org/example/App.java
+++ zoo/src/main/java/org/example/App.java
@@ -1,13 +1,36 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.example.animals.*;
+import org.example.food.Beef;
+import org.example.food.Fish;
+import org.example.food.Grass;
+import org.example.food.Meat;
+
+public class App {
+    public static void main(String[] args) {
+        // animals
+        Camel camel = new Camel("Isabel", 10, false);
+        Tiger tiger = new Tiger("Leo", 20, true);
+        Dolphin dolphin = new Dolphin("Rocket", 15, false);
+        Eagle eagle = new Eagle("Johny", 37, true);
+        Horse horse = new Horse("Marry", 13, false);
+
+        //food
+        Beef beef = new Beef(100);
+        Fish fish = new Fish(20);
+        Grass grass = new Grass(50);
+
+        System.out.println(tiger);
+        tiger.walk();
+        tiger.eat(fish, 30);
+        tiger.eat(beef, 70);
+        tiger.eat(beef,70);
+
+        horse.eat(grass, 50);
+        eagle.eat(fish, 10);
+        dolphin.eat(fish, 10);
+
+        dolphin.swim();
+        eagle.fly();
     }
 }
---
+++ zoo/src/main/java/org/example/food/Meat.java
@@ -0,0 +1,7 @@
+package org.example.food;
+
+public abstract class Meat extends Food {
+    public Meat(int amount) {
+        super(amount);
+    }
+}
